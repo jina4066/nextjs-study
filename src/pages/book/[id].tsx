d6c8480ea106fd39314bf231a12beefce0e21483
김지나 <jina4066@naver.com>
2025-05-22 15:44:49 +0900

    feat: 동적 경로 생성

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,10 +1,21 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      // url 파라미터 값은 반드시 문자열로 명시를 해야 next가 읽어올 수 있다
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    //만약 브라우저에서 path값으로 설정하지 않은 경로로 접속 요청을 할 때의 대비책
+    fallback: false, //404
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id; //! 타입을 사용해 undefined이 아닐 것이라고 단언(파라미터가 없다면 들어올 수 없는 url)
   const book = await fetchOneBook(Number(id)); // url 파라미터로써 불러온 id는 기본적으로 스트링 타입 -> Number로 타입 변환 필요
 
@@ -15,7 +26,7 @@ export const getServerSideProps = async (
 
 export default function Page({
   book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   if (!book) return "문제가 발생했습니다. 다시 시도해주세요";
 
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
