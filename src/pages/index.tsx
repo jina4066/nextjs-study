7b5814ef2c152e6f4ec96fddc82e28abd319cb51
김지나 <jina4066@naver.com>
2025-05-22 15:33:42 +0900

    feat: SSG 방식을 동적 경로에 적용하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,16 @@ import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import { InferGetStaticPropsType } from "next";
+
+// getStaticProps -> SSG
+// getServerSideProps -> SSR
 
 //컴포넌트보다 먼저 실행이 되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
+  console.log("인덱스페이지");
   // 병렬로 api request가 동시에 발송 -> 더 빠르게 페이지 렌더링
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
@@ -26,11 +30,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log("지나");
-  console.log(recoBooks);
-  console.log("?");
-
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,23 +1,39 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import BookItem from "@/components/book-item";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
+import { useRouter } from "next/router";
+import { BookData } from "@/types";
+
 //context 객체에는 브라우저로부터 받은 요청에 대한 모든 정보 포함
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
+// export const getStaticProps = async (
+//   context: GetStaticPropsContext
+// ) => {
+//     // 빌드타임에 쿼리를 알아낼 수 없기에 쿼리속성이 없다.
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
 
-  return {
-    props: { books },
+//   return {
+//     props: { books },
+//   };
+// };
+
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
   };
-};
 
-export default function Page({
-  books,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+    }
+  }, [q]);
   return (
     <div>
       {books.map((book) => (
